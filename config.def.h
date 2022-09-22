0be6118c4538f3aa3c1e09494d078453b9756030
yogeshdcool <dyogesh.official@gmail.com>
2022-09-22 19:10:43 +0530

    antialias,autohint

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "monospace:size=10";
+static char font[] ="JetBrainsMono Nerd Font:style:medium:size=10:antialias=true:hinting=true";
 static const char *fonts[] = {
 	font,
 	"monospace:size=10",
@@ -17,14 +17,17 @@ static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
 static char selfgcolor[]  = "#eeeeee";
 static char selbgcolor[]  = "#005577";
+static char outfgcolor[]  = "#000000";
+static char outbgcolor[]  = "#00ffff";
+static char hlfgcolor[]  = "#ffc978";
 static char *colors[SchemeLast][2] = {
 
 	/*     fg         bg       */
 	[SchemeNorm] = { normfgcolor, normbgcolor },
 	[SchemeSel]  = { selfgcolor,  selbgcolor  },
-	[SchemeOut]  = { "#000000",   "#00ffff" },
-	[SchemeSelHighlight] = { "#ffc978", selbgcolor },
-	[SchemeNormHighlight] = { "#ffc978", normbgcolor },
+	[SchemeOut]  = { outfgcolor,  outbgcolor },
+	[SchemeSelHighlight] = { hlfgcolor, selbgcolor },
+	[SchemeNormHighlight] = { hlfgcolor, normbgcolor },
 	
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -40,10 +43,11 @@ static const char worddelimiters[] = " ";
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-	{ "font",        STRING, &font },
-	{ "normfgcolor", STRING, &normfgcolor },
-	{ "normbgcolor", STRING, &normbgcolor },
-	{ "selfgcolor",  STRING, &selfgcolor },
-	{ "selbgcolor",  STRING, &selbgcolor },
-	{ "prompt",      STRING, &prompt },
+	{ "color7", STRING, &normfgcolor },
+	{ "color0", STRING, &normbgcolor },
+	{ "color7",  STRING, &selfgcolor },
+	{ "color1",  STRING, &selbgcolor },
+	{ "color7",  STRING, &outfgcolor },
+	{ "color14",  STRING, &outfgcolor },
+	{ "color5",  STRING, &hlfgcolor },
 };
